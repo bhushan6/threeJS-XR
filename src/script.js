4d4eb70666482a4a7504ec0dbcd207c5eb65d347
Bhushan Wagh <bwagh@paninian.com>
2022-12-20 23:04:08 +0530

    Added: Ar with glb model

--- src/script.js
+++ src/script.js
@@ -1,95 +1,160 @@
-import "./style.css";
+// import "./styles.css";
+
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { ARButton } from "three/examples/jsm/webxr/ARButton.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
-/**
- * Base
- */
-// Canvas
-const canvas = document.querySelector("canvas.webgl");
+let container;
+let camera, scene, renderer;
+let controller1, controller2;
 
-// Scene
-const scene = new THREE.Scene();
+let raycaster;
 
-/**
- * Sizes
- */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
+const intersected = [];
+const tempMatrix = new THREE.Matrix4();
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+let group;
 
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
+init();
+animate();
+
+function init() {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    10
+  );
+  camera.position.set(0, 0, 3);
+
+  const controls = new OrbitControls(camera, container);
+  controls.minDistance = 0;
+  controls.maxDistance = 8;
+
+  scene.add(new THREE.HemisphereLight(0x808080, 0x606060));
+
+  const light = new THREE.DirectionalLight(0xffffff);
+  light.position.set(0, 6, 0);
+  scene.add(light);
+
+  group = new THREE.Group();
+  scene.add(group);
+  new GLTFLoader().load("/SheenChair.glb", function (gltf) {
+    group.add(gltf.scene);
+  });
+
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.xr.enabled = true;
+  container.appendChild(renderer.domElement);
+
+  document.body.appendChild(ARButton.createButton(renderer));
+
+  // controllers
+
+  controller1 = renderer.xr.getController(0);
+  controller1.addEventListener("selectstart", onSelectStart);
+  controller1.addEventListener("selectend", onSelectEnd);
+  scene.add(controller1);
+
+  controller2 = renderer.xr.getController(1);
+  controller2.addEventListener("selectstart", onSelectStart);
+  controller2.addEventListener("selectend", onSelectEnd);
+  scene.add(controller2);
+
+  raycaster = new THREE.Raycaster();
+
+  //
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-camera.position.x = 1;
-camera.position.y = 1;
-camera.position.z = 1;
-scene.add(camera);
+function onSelectStart(event) {
+  const controller = event.target;
 
-// Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
+  const intersections = getIntersections(controller);
 
-/**
- * Cube
- */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: 0xff00f0 })
-);
-scene.add(cube);
+  if (intersections.length > 0) {
+    const intersection = intersections[0];
 
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-  antialias: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    const object = intersection.object;
+    object.material.emissive.b = 1;
+    controller.attach(object);
 
-/**
- * Animate
- */
-const clock = new THREE.Clock();
-let lastElapsedTime = 0;
+    controller.userData.selected = object;
+  }
+}
 
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-  const deltaTime = elapsedTime - lastElapsedTime;
-  lastElapsedTime = elapsedTime;
+function onSelectEnd(event) {
+  const controller = event.target;
 
-  // Update controls
-  controls.update();
+  if (controller.userData.selected !== undefined) {
+    const object = controller.userData.selected;
+    object.material.emissive.b = 0;
+    group.attach(object);
 
-  // Render
+    controller.userData.selected = undefined;
+  }
+}
+
+function getIntersections(controller) {
+  tempMatrix.identity().extractRotation(controller.matrixWorld);
+
+  raycaster.ray.origin.setFromMatrixPosition(controller.matrixWorld);
+  raycaster.ray.direction.set(0, 0, -1).applyMatrix4(tempMatrix);
+
+  return raycaster.intersectObjects(group.children, false);
+}
+
+function intersectObjects(controller) {
+  // Do not highlight when already selected
+
+  if (controller.userData.selected !== undefined) return;
+
+  const intersections = getIntersections(controller);
+
+  if (intersections.length > 0) {
+    const intersection = intersections[0];
+
+    const object = intersection.object;
+    object.material.emissive.r = 1;
+    intersected.push(object);
+  }
+}
+
+function cleanIntersected() {
+  while (intersected.length) {
+    const object = intersected.pop();
+    object.material.emissive.r = 0;
+  }
+}
+
+//
+
+function animate() {
+  renderer.setAnimationLoop(render);
+}
+
+function render() {
+  cleanIntersected();
+
+  intersectObjects(controller1);
+  intersectObjects(controller2);
+
   renderer.render(scene, camera);
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-
-tick();
+}
